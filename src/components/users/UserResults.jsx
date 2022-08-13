02fa7a26b4e276b9917eba58474801e2519c9689
Aryman <87179546+rawat-aryman@users.noreply.github.com>
2022-08-13 21:14:49 +0530

    adding userItem component with styling

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import {useEffect , useState} from 'react';
+import Spinner from '../layout/Spinner';
+import UserItem from './UserItem';
 
 function UserResults() {
 
@@ -27,13 +29,13 @@ function UserResults() {
         return (
             <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
                 {users.map((user) => {
-                    return <h3>{user.login}</h3>
+                    return <UserItem key={user.id} user={user}/>
                 })}
             </div>
         )
     }
     else {
-        return <h2>Loading</h2>
+        return <Spinner /> ;
     }
 }
 
